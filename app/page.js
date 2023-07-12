5ad4be7ce9ee5c6d8524f10bb86ba53b58bf55a4
Raymund Lapuz <raymund.lapuz.x@bulsu.edu.ph>
2023-07-13 03:12:54 +0800

    add login

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
-import Sidebar from './components/sidebar/Sidebar';
 import Footer from './components/footer/Footer';
+import Link from 'next/link';
+import './globals.css'
 
 export const metadata = {
   title: 'Raymund | Home',
@@ -8,9 +9,29 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className='flex flex-col'>
-      <Sidebar />
-      <Footer />
-    </div>
+    <>
+      <div className='bsu'>
+        <Image
+          src='/bsu.png'
+          alt='bsu bg'
+          layout='fill'
+          objectFit='cover'
+        />
+      </div>
+      <div className=''>
+        <div className='flex flex-col gap-5 items-center justify-center h-screen'>
+          <h1 className='text-xl font-bold '>Automation Signatory</h1>
+          <Link
+            href="/login"
+            className="mb-3 inline-block w-auto md:w-1/6 rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] lot text-center"
+            type="button"
+            data-te-ripple-init
+            data-te-ripple-color="light">
+            Log in
+          </Link>
+        </div>
+        <Footer />
+      </div>
+    </>
   )
 }
--- app/upload/page.jsx
+++ app/upload/page.jsx
@@ -1,22 +1,21 @@
+import Drag from "../components/drag & drop/Drag";
 import Navbar from "../components/navbar/Navbar";
 
-
 export const metadata = {
-  title: 'Raymund | Upload',
+  title: "Raymund | Upload",
 };
 const page = () => {
   return (
     <>
-      <div className='w-full flex flex-col gap-7'>
+      <div className="w-full flex flex-col gap-7">
         <Navbar />
-        <main className=''>
+        <main className="">
           <h1 className="font-medium">Upload Files</h1>
-          <div className='mt-10'>
-          </div>
+          <div className="mt-10">{/* <Drag /> */}</div>
         </main>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
